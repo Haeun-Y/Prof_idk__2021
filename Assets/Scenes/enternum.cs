e70e11b3bdff407d82b305c2dd7f2465905d980f
ssundori <b_bomb030@naver.com>
2021-08-11 03:04:53 +0900

    210811 기존 프로젝트 import

---
+++ Assets/Scenes/enternum.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class enternum : MonoBehaviour
+{
+    public Text answer;
+    string input = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        answer.text = null;
+    }
+
+    // Update is called once per frame
+    public void enter_1()
+    {
+        answer.text = answer.text + "1";
+    }
+
+    public void enter_2()
+    {
+        answer.text = answer.text + "2";
+    }
+
+    public void enter_3()
+    {
+        answer.text = answer.text + "3";
+    }
+
+    public void enter_4()
+    {
+        answer.text = answer.text + "4";
+    }
+
+    public void enter_5()
+    {
+        answer.text = answer.text + "5";
+    }
+
+    public void enter_6()
+    {
+        answer.text = answer.text + "6";
+    }
+
+    public void enter_7()
+    {
+        answer.text = answer.text + "7";
+    }
+
+    public void enter_8()
+    {
+        answer.text = answer.text + "8";
+    }
+
+    public void trash()
+    {
+        input = null;
+        answer.text = input;
+    }
+}
---
+++ Assets/Scenes/fontchange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fontchange : MonoBehaviour
+{
+    public Image TestImage;
+    public Sprite TestSprite;
+
+    public void ChangeImage()
+    {
+        TestImage.sprite = TestSprite;
+    }
+}
