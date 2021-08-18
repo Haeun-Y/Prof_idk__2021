8661c9b521e92e56194170eaba16e21fc16f5638
ssundori <b_bomb030@naver.com>
2021-08-18 15:24:53 +0900

    210818 stage1 수정
    교수님 전화번호 수정, 몰라 추가

--- Assets/Scripts/enternum.cs
+++ Assets/Scripts/enternum.cs
@@ -8,6 +8,8 @@ public class enternum : MonoBehaviour
     public GameObject notion;
     public Text answer;
     string input = null;
+    public GameObject refresh;
+    public GameObject img_f5;
 
     // Start is called before the first frame update
     void Start()
@@ -18,41 +20,57 @@ public class enternum : MonoBehaviour
     // Update is called once per frame
     public void enter_0()
     {
+        refresh.gameObject.SetActive(false);
+        img_f5.gameObject.SetActive(true);
         answer.text = answer.text + "0";
     }
 
     public void enter_1()
     {
+        refresh.gameObject.SetActive(false);
+        img_f5.gameObject.SetActive(true);
         answer.text = answer.text + "1";
     }
 
     public void enter_2()
     {
+        refresh.gameObject.SetActive(false);
+        img_f5.gameObject.SetActive(true);
         answer.text = answer.text + "2";
     }
 
     public void enter_3()
     {
+        refresh.gameObject.SetActive(false);
+        img_f5.gameObject.SetActive(true);
         answer.text = answer.text + "3";
     }
 
     public void enter_4()
     {
+        refresh.gameObject.SetActive(false);
+        img_f5.gameObject.SetActive(true);
         answer.text = answer.text + "4";
     }
 
     public void enter_5()
     {
+        refresh.gameObject.SetActive(false);
+        img_f5.gameObject.SetActive(true);
         answer.text = answer.text + "5";
     }
 
     public void enter_6()
     {
+        refresh.gameObject.SetActive(false);
+        img_f5.gameObject.SetActive(true);
         answer.text = answer.text + "6";
     }
 
     public void enter_7()
     {
+        refresh.gameObject.SetActive(false);
+        img_f5.gameObject.SetActive(true);
         answer.text = answer.text + "7";
 
         if(answer.text == "01019872887")
@@ -63,11 +81,15 @@ public class enternum : MonoBehaviour
 
     public void enter_8()
     {
+        refresh.gameObject.SetActive(false);
+        img_f5.gameObject.SetActive(true);
         answer.text = answer.text + "8";
     }
 
     public void enter_9()
     {
+        refresh.gameObject.SetActive(false);
+        img_f5.gameObject.SetActive(true);
         answer.text = answer.text + "9";
     }
 
--- Assets/Scripts/fontchange.cs
+++ Assets/Scripts/fontchange.cs
@@ -5,11 +5,51 @@ using UnityEngine.UI;
 
 public class fontchange : MonoBehaviour
 {
-    public Image TestImage;
-    public Sprite TestSprite;
+    public GameObject font1, font2, font3, font4, font5;
+    public GameObject f51, f52, f53, f54, f55;
+    public GameObject btn_num;
 
-    public void ChangeImage()
+    public void change_font1to2()
     {
-        TestImage.sprite = TestSprite;
+        font1.gameObject.SetActive(false);
+        font2.gameObject.SetActive(true);
+        f51.gameObject.SetActive(false);
+        f52.gameObject.SetActive(true);
     }
+
+    public void change_font2to3()
+    {
+        font2.gameObject.SetActive(false);
+        font3.gameObject.SetActive(true);
+        f52.gameObject.SetActive(false);
+        f53.gameObject.SetActive(true);
+    }
+
+    public void change_font3to4()
+    {
+        font3.gameObject.SetActive(false);
+        font4.gameObject.SetActive(true);
+        f53.gameObject.SetActive(false);
+        f54.gameObject.SetActive(true);
+    }
+
+    public void change_font4to5()
+    {
+        font4.gameObject.SetActive(false);
+        font5.gameObject.SetActive(true);
+        f54.gameObject.SetActive(false);
+        f55.gameObject.SetActive(true);
+        btn_num.gameObject.SetActive(true);
+    }
+
+    public void change_font5to1()
+    {
+        font5.gameObject.SetActive(false);
+        font1.gameObject.SetActive(true);
+        f55.gameObject.SetActive(false);
+        f51.gameObject.SetActive(true);
+        btn_num.gameObject.SetActive(false);
+    }
+
+
 }
